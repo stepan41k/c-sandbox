1f8a4fbaedd408262fcd7f52fd95e6b6d13af66e
stepan41k <russia.zont@gmail.com>
2026-05-06 01:24:24 +0300

    feat: add sum of two

---
+++ go/add/add.s
@@ -0,0 +1,6 @@
+TEXT ·Add(SB), $0-24
+    MOVQ a+0(FP), AX
+    ADDQ b+8(FP), AX
+    MOVQ AX, ret+16(FP)
+    RET
+    
---
+++ go/add/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func Add(a, b int64) int64 
+
+func main() {
+	fmt.Println(Add(10, 15))
+}
