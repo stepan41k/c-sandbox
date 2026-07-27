f84d544096cc138416b83743919364e7c8319087
stepan41k <russia.zont@gmail.com>
2026-07-27 23:16:10 +0300

    feat(c): add strings example

---
+++ c/tasks/3_strings/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    char *str1 = "Hello";
+
+    char str2[] = "Hello";
+    str2[0] = 'H';
+
+    char *str3 = malloc(10 * sizeof(char));
+    strcpy(str3, "Gopher");
+
+    printf("%s: %d\n", str3, strlen(str3));
+
+    free(str3);
+
+    return 0;
+}
