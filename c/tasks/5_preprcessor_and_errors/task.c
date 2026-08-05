06d32152605b1fab459877f1e539ae21d65c15bd
stepan41k <russia.zont@gmail.com>
2026-08-05 23:33:00 +0300

    feat(c): add facade for task

--- c/tasks/5_preprcessor_and_errors/task.c
+++ c/tasks/5_preprcessor_and_errors/task.c
@@ -1,4 +1,17 @@
-
-
+#include <stdio.h>
 #define MAX_NAME 50
 
+typedef struct {
+    int id;
+    char name[MAX_NAME];
+    float salary;
+} Employee;
+
+Employee* create_employee(int id, char* name, float salary) {
+    
+}
+
+int main() {
+    Employee e = {1, "Igor", 15000.0};
+    printf("%ld\n", sizeof(Employee));
+}
